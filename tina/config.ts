5dff31de094361764b8027d4ad183a9a725e523a
maeligg <maelbrunet@gmail.com>
2023-11-24 14:33:54 +0100

    Replace NetlifyCMS by TinaCMS

---
+++ tina/config.ts
@@ -0,0 +1,82 @@
+import {defineConfig} from 'tinacms'
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  'master'
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: 'admin',
+    publicFolder: 'dist',
+  },
+  media: {
+    tina: {
+      mediaRoot: 'src/images/project-thumbnails',
+      publicFolder: '',
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: 'projects',
+        label: 'Projects',
+        path: 'src/projects',
+        fields: [
+          {
+            type: 'string',
+            name: 'title',
+            label: 'Title',
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body',
+            isBody: true,
+            required: true,
+          },
+          {
+            type: 'datetime',
+            name: 'date',
+            label: 'Date',
+            required: true,
+          },
+          {
+            type: 'string',
+            name: 'category',
+            label: 'Category',
+            required: true,
+          },
+          {
+            type: 'image',
+            name: 'thumbnail',
+            label: 'Thumbnail',
+            required: true,
+          },
+          {
+            type: 'string',
+            name: 'url',
+            label: 'URL',
+          },
+          {
+            type: 'string',
+            name: 'code',
+            label: 'Code',
+          },
+        ],
+      },
+    ],
+  },
+})
